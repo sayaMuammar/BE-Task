dcedf74b3ff1d0f6aded9343fd2eeb041fb18c42
sayaMuammar <muammarbaim2003@gmail.com>
2022-09-06 09:57:14 +0700

    revisi problemSolving1

--- ProblemSolving1.java
+++ ProblemSolving1.java
@@ -8,28 +8,15 @@ public class ProblemSolving1 {
     public static void main(String[] args) {
         
         
-     int nums[] = {3,7,12,15};
-     int target = 15;
-     if(nums[0]+nums[1]== target){
-         System.out.println(0+", "+1);
-     }
-     else if (nums[1]+nums[2]== target){
-         System.out.println(1+", "+2);
-     }
-     else if (nums[2]+nums[3]== target){
-         System.out.println(2+", "+3);
-     }
-     else if (nums[0]+nums[3]== target){
-         System.out.println(0+", "+3);
-     }
-     else if (nums[0]+nums[2]== target){
-         System.out.println(0+", "+2);
-     }
-     else if (nums[1]+nums[3]== target){
-         System.out.println(2+", "+3);
-     }
-     else{
-         System.out.println("target tidak cocok");
-     }
+     int[] nums = {2, 5, 13, 15};
+        int target = 7;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.println("[ " + i +", " + j + " ]");
+                }
+            }
+        }
     }
 }
